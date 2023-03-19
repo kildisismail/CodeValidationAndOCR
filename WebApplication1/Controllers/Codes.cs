0ee4dbbf4d5b2d679b1b54b7e6b4323cfa63dbd5
İsmail Kıldiş <kildisismail@gmail.com>
2023-03-19 11:34:53 +0300

    codes controller fix and refactor.

--- WebApplication1/Controllers/Codes.cs
+++ WebApplication1/Controllers/Codes.cs
@@ -6,6 +6,8 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class Codes : ControllerBase
     {
+        string characters = "ACDEFGHKLMNPRTXYZ";
+        string numbers = "234579";
 
         private readonly ILogger<Codes> _logger;
 
@@ -14,9 +16,42 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
+        [HttpGet("generated")]
+        public async Task<IActionResult> GenerateCode()
+        {
+            Random random = new Random();
+            //code1 ve code2 olarak ayrý ayrý 2 kod üretilir.
+            //const olarak tanýmlanan characters keyine göre random 3 karakter üretilir.
+            string code1 = new string(Enumerable.Repeat(characters, 3).Select(s => s[random.Next(s.Length)]).ToArray());
+            string code2 = new string(Enumerable.Repeat(characters, 3).Select(s => s[random.Next(s.Length)]).ToArray());
+
+            //const olarak tanýmlanan numbers keyine göre random 2 karakter üretilir.
+            string numbers1 = new string(Enumerable.Repeat(numbers, 2).Select(s => s[random.Next(s.Length)]).ToArray());
+
+            //üretilen code1 ve code2 nin sonuna number1 eklenip birleþtirilerek 8 haneli bir kod üretilir.
+            string concatCode = code1 + numbers1.Substring(0, 1) + code2 + numbers1.Substring(1, 1);
+
+            //üretilen 8 haneli kod bu yardýmcý method ile kodun karakterlerinin ascii deðerleri hesaplarak listeye eklenir.
+            List<int> codes = AsciiCharControl(concatCode);
+
+            //8 haneli kodun ascii deðerlerinin 2 - 4 - 6 ve 1 - 3 - 7 haneleri toplanarak mod2 alýnýr. Eþit ise kod üretilir.
+            //Deðil ise GenerateCode methodu tekrar çalýþarak recursive þekilde valid kod generate edilmiþ olur. 
+            bool isValid = CalculateCodes(codes);
+
+            if (isValid)
+            {
+                return Ok(concatCode);
+            }
+            else
+            {
+                return await GenerateCode();
+            }
+        }
+
         [HttpPost("verified")]
         public IActionResult VerifyCode(string code)
         {
+            //boþ kod girildiðinde doðrudan code parameters is not null!! dönülür
             if (string.IsNullOrEmpty(code))
             {
                 return BadRequest("code parameters is not null!!");
@@ -24,13 +59,17 @@ namespace WebApplication1.Controllers
 
             try
             {
-                int asciiSum = code.Sum(c => (int)c);
+                //kullanýcýnýn girdiði 8 haneli kod bu yardýmcý method ile kodun karakterlerinin ascii deðerleri hesaplarak listeye eklenir.
+                List<int> codes = AsciiCharControl(code);
 
-                if (asciiSum != 500)
+                //kullanýcýnýn girdiði 8 haneli kodun ascii deðerlerinin 2 - 4 - 6 ve 1 - 3 - 7 haneleri toplanarak mod2 alýnýr. Eþit ise true, deðil ise false döner.
+                bool isValid = CalculateCodes(codes);
+
+                if (isValid)
                 {
-                    return NotFound(false);
+                    return Ok(true);
                 }
-                return Ok(true);
+                return NotFound(false);
             }
             catch (Exception ex)
             {
@@ -38,27 +77,31 @@ namespace WebApplication1.Controllers
             }
         }
 
-        [HttpGet("generated")]
-        public async Task<IActionResult> GetKodlar()
+        private static List<int> AsciiCharControl(string code)
         {
-            string characters = "ACDEFGHKLMNPRTXYZ234579";
-            Random random = new Random();
-
-            string code1 = new string(Enumerable.Repeat(characters, 4).Select(s => s[random.Next(s.Length)]).ToArray());
-            string code2 = new string(Enumerable.Repeat(characters, 4).Select(s => s[random.Next(s.Length)]).ToArray());
-            string concatCode = code1 + code2;
-            int totalAsciiValue = concatCode.Sum(c => (int)c);
-
-            if (totalAsciiValue == 500)
+            List<int> codes = new List<int>();
+            int codesAsciiValues;
+            // kodun karakterlerinin ascii deðerleri hesaplarak listeye eklenir.
+            for (int i = 0; code.Length > i; i++)
             {
-                return Ok(concatCode);
+                codesAsciiValues = Convert.ToInt32(Convert.ToChar(code.Substring(i, 1)));
+                codes.Add(codesAsciiValues);
             }
-            else
-            {
-                return await GetKodlar();
-            }
+
+            return codes;
         }
 
+        private bool CalculateCodes(List<int> numbers)
+        {
+            //8 haneli kodun ascii deðerlerinin 2 - 4 - 6 ve 1 - 3 - 7 haneleri toplanarak mod2 alýnýr. Eþit ise true, deðil ise false döner.
+            int evenNumbers = numbers[2] + numbers[4] + numbers[6];
+            int oddNumbers = numbers[1] + numbers[3] + numbers[7];
+            if (evenNumbers % 2 == oddNumbers % 2)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 
 }
